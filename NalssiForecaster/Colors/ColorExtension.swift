ab2ba1118187cbf91bdc87e407c568d3504499f7
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-05-31 13:31:08 -0500

    Add colors in Assets Create Color folder with ColorExtension file to make them a static property

---
+++ NalssiForecaster/Colors/ColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtension.swift
+//  NalssiForecaster
+//
+//  Created by Kevin Mattocks on 5/31/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primaryColor = Color("primaryColor")
+    static let backgroundColor = Color("backgroundColor")
+}
