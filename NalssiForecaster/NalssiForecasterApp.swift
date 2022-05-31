7e2a23517cc47681a431dd89c8f84b751dced2e2
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-05-31 11:30:30 -0500

    Initial Commit

---
+++ NalssiForecaster/NalssiForecasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NalssiForecasterApp.swift
+//  NalssiForecaster
+//
+//  Created by Kevin Mattocks on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct NalssiForecasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
