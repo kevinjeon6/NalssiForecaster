7d7113d1f1654472ea5a31630d2c1c656ac5dbee
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-05-31 13:18:13 -0500

    Create logic for Weather model for OpenWeatherMap API

--- NalssiForecaster/Model/Weather.swift
+++ NalssiForecaster/Model/Weather.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+
+struct Weather: Decodable {
+    var current: Current
+    var hourly: [Current]
+    var daily: [Daily]
+    
+    //Hourly is an array of weather responses (i.e. Current). It parses the data because the array is similar to Current properties
+}
+
+struct Current: Decodable, Identifiable {
+    let id = UUID()
+    var dt: Double
+    var temp: Double
+    var feels_like: Double
+    var weather: [WeatherInfo]
+}
+
+struct WeatherInfo: Decodable {
+    var description: String
+}
+
+struct Daily: Decodable, Identifiable {
+    
+    let id = UUID()
+    var dt: Double
+    var temp: DailyTemp
+}
+
+struct DailyTemp: Decodable {
+    var min: Double
+    var max: Double
+}
